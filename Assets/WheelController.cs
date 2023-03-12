80373e89477168a0145ff9659388bc6f5aeac40d
HaichuanLi <123408660+HaichuanLi@users.noreply.github.com>
2023-03-11 20:51:17 -0500

    Voiture presque complete

---
+++ Assets/WheelController.cs
@@ -0,0 +1,50 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WheelController : MonoBehaviour
+{
+    [SerializeField] WheelCollider FrontRightWheel;
+    [SerializeField] WheelCollider FrontLeftWheel;
+    [SerializeField] WheelCollider RearRightWheel;
+    [SerializeField] WheelCollider RearLeftWheel;
+
+    public float acceleration = 100f;
+    public float breakingForce = 60f;
+    public float maxTurnAngle = 45f;
+
+
+    private float currentAcceleration = 0f;
+    private float currentBrakeForce = 0f;
+    private float currentTurnAngle= 0f;
+
+    private void FixedUpdate()
+    {
+        currentAcceleration = acceleration * Input.GetAxis("Vertical");
+
+        if (Input.GetKey(KeyCode.Space))
+            currentBrakeForce = breakingForce;
+
+        
+        else
+            currentBrakeForce = 0f;
+
+        
+
+        FrontRightWheel.motorTorque = currentAcceleration;
+        FrontLeftWheel.motorTorque = currentAcceleration;
+
+        FrontRightWheel.brakeTorque = currentBrakeForce;
+        FrontLeftWheel.brakeTorque = currentBrakeForce;
+        RearRightWheel.brakeTorque = currentBrakeForce;
+        RearLeftWheel.brakeTorque = currentBrakeForce;
+
+        currentTurnAngle = maxTurnAngle * Input.GetAxis("Horizontal");
+        FrontLeftWheel.steerAngle = currentTurnAngle;
+        FrontRightWheel.steerAngle = currentTurnAngle;
+
+        if (Input.GetKey(KeyCode.W))
+            this.transform.position = new Vector3(35, 14, 31);
+    }
+
+}
