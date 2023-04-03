2e4e87e799136990aef3971d2df660154177751c
MonsieurLahlou <2132110@bdeb.qc.ca>
2023-04-03 17:06:21 -0400

    LumiereVoiture
    LumiereVoiture

--- Assets/ArreLumiere.cs
+++ Assets/ArreLumiere.cs
@@ -7,9 +7,10 @@ using UnityEngine;
 public class ArreLumiere : MonoBehaviour
 {
     private int compteur = 0;
-    private bool stop = false, toucheTrigger1 = false, toucheTrigger2 = false;
+    private bool stop = false, toucheTrigger1 = false, toucheTrigger2 = false, arretconfirme = false;
     private float posimage1x, posimage1z, posimage2x, posimage2z;
     public float speed;
+    public GameObject lumiere;
     // Start is called before the first frame update
     void Start()
     {
@@ -41,7 +42,7 @@ public class ArreLumiere : MonoBehaviour
         }
         else if (other.name.Equals("Trigger2") && !toucheTrigger2)
         {
-            toucheTrigger2 |= true;
+            toucheTrigger2 = true;
             compteur++;
             Debug.Log("S");
         }
@@ -55,11 +56,19 @@ public class ArreLumiere : MonoBehaviour
         {
             if (speed == 0)
             {
-                Debug.Log("Yes");
+                arretconfirme = true;
+            }
+            if (lumiere.GetComponent<ChangerLumiere>().rouge.active && arretconfirme)
+            {
+                Debug.Log("Bien vu!");
                 stop = true;
                 toucheTrigger1 = false;
-                toucheTrigger2 = true;
-            }
+                toucheTrigger2 = false;
+                arretconfirme= false;
+            } else if ((lumiere.GetComponent<ChangerLumiere>().vert.active && arretconfirme)||(lumiere.GetComponent<ChangerLumiere>().rouge.active && !arretconfirme))
+            {
+                Debug.Log("Tu tripppp!");
+            } 
         }
         posimage1x = posimage2x;
         posimage1z = posimage2z;
